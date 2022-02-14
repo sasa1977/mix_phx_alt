50d4f22a14d6f479a0a2ee22e419413ffb3b5696
Saša Jurić <sasa.juric@gmail.com>
2022-02-15 00:02:30 +0100

    Remove needless comments

--- mix.exs
+++ mix.exs
@@ -14,9 +14,6 @@ defmodule Demo.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {Demo.Application, []},
@@ -24,13 +21,9 @@ defmodule Demo.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
       {:credo, "~> 1.6", runtime: false},
@@ -52,12 +45,6 @@ defmodule Demo.MixProject do
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup"],
