4c233266d752b1a3ea60261a9a62b05309b8911e
Saša Jurić <sasa.juric@gmail.com>
2022-02-14 15:34:39 +0100

    Drop seeds

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,5 @@
 [
   import_deps: [:ecto, :ecto_sql, :phoenix],
-  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"],
   subdirectories: ["priv/*/migrations"]
 ]
