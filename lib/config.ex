e608a3482d1aa2185403e6f769a52db3a8dbda25
Saša Jurić <sasa.juric@gmail.com>
2022-02-15 21:28:28 +0100

    Extract repo config

--- lib/config.ex
+++ lib/config.ex
@@ -15,12 +15,28 @@ defmodule Demo.Config do
   @spec site_host :: String.t()
   def site_host, do: os_env("SITE_HOST")
 
+  @spec db_url :: String.t()
+  def db_url, do: os_env("DB_URL")
+
+  @spec db_pool_size :: pos_integer
+  def db_pool_size, do: String.to_integer(os_env("DB_POOL_SIZE"))
+
+  @spec db_ipv6? :: boolean
+  def db_ipv6?, do: os_env("DB_IPV6") == "true"
+
   defp os_env(name), do: System.get_env(name) || default(name, mix_env())
 
   defp default("SITE_HOST", mix_env) when mix_env in ~w/dev test/a, do: "localhost"
 
   defp default("SECRET_KEY_BASE", mix_env) when mix_env in ~w/dev test/a,
     do: "6SQyoN0wWViSTd5UaarW/wZsqTX0sFgYqYfGZpehG2s6kCwJOSiVVaiLBUO5oUdB"
+
+  defp default("DB_URL", mix_env) when mix_env in ~w/dev test/a,
+    do: "ecto://postgres:postgres@localhost/demo_#{mix_env}"
+
+  defp default("DB_POOL_SIZE", mix_env) when mix_env in ~w/dev test/a, do: "10"
+
+  defp default("DB_IPV6", _mix_env), do: "false"
 
   defp default(var, _mix_env), do: raise("#{var} environment variable is not set")
 end
--- lib/core/repo.ex
+++ lib/core/repo.ex
@@ -7,41 +7,19 @@ defmodule Demo.Core.Repo do
   def init(_context, config) do
     opts =
       config
-      |> deep_merge(pool_size: 10)
+      |> deep_merge(
+        url: Demo.Config.db_url(),
+        pool_size: Demo.Config.db_pool_size(),
+        socket_options: if(Demo.Config.db_ipv6?(), do: [:inet6], else: [])
+      )
       |> deep_merge(repo_opts(Demo.Config.mix_env()))
 
     {:ok, opts}
   end
 
-  defp repo_opts(:dev) do
-    [
-      url: "ecto://postgres:postgres@localhost/demo_dev"
-    ]
-  end
-
-  defp repo_opts(:test) do
-    [
-      url: "ecto://postgres:postgres@localhost/demo_test#{System.get_env("MIX_TEST_PARTITION")}",
-      pool: Ecto.Adapters.SQL.Sandbox
-    ]
-  end
-
-  defp repo_opts(:prod) do
-    database_url =
-      System.get_env("DATABASE_URL") ||
-        raise """
-        environment variable DATABASE_URL is missing.
-        For example: ecto://USER:PASS@HOST/DATABASE
-        """
-
-    maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
-
-    [
-      url: database_url,
-      pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-      socket_options: maybe_ipv6
-    ]
-  end
+  defp repo_opts(:dev), do: [show_sensitive_data_on_connection_error: true]
+  defp repo_opts(:test), do: [pool: Ecto.Adapters.SQL.Sandbox]
+  defp repo_opts(:prod), do: []
 
   defp deep_merge(list1, list2) do
     # Config.Reader.merge requires a top-level format of `key: kw-list`, so we're using the `:opts` key
