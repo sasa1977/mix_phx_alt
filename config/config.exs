32b9a47f600b0d042587d5ff10b3c384c40a2e2e
Saša Jurić <sasa.juric@gmail.com>
2022-02-15 21:45:21 +0100

    Consolidate compile-time configs

--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,5 @@
 import Config
 
-config :demo, mix_env: config_env()
-
-config :demo,
-  ecto_repos: [Demo.Core.Repo]
-
 config :esbuild,
   version: "0.14.0",
   default: [
@@ -15,9 +10,27 @@ config :esbuild,
   ]
 
 config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
+  format:
+    if(config_env() == :dev,
+      do: "[$level] $message\n",
+      else: "$time $metadata[$level] $message\n"
+    ),
+  level: Map.fetch!(%{dev: :debug, test: :warn, prod: :info}, config_env()),
   metadata: [:request_id]
 
-config :phoenix, :json_library, Jason
+config :phoenix,
+  json_library: Jason,
+  plug_init_mode: if(config_env() == :prod, do: :compile, else: :runtime),
+  stacktrace_depth: if(config_env() == :dev, do: 20)
 
-import_config "#{config_env()}.exs"
+# demo app
+
+config :demo,
+  mix_env: config_env(),
+  ecto_repos: [Demo.Core.Repo]
+
+if config_env() == :dev do
+  config :demo, Demo.Interface.Endpoint,
+    code_reloader: true,
+    debug_errors: true
+end
--- config/test.exs
+++
@@ -1,5 +0,0 @@
-import Config
-
-config :logger, level: :warn
-
-config :phoenix, :plug_init_mode, :runtime
