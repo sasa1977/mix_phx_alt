339f6edf93a1cbc00f338d1cc783eccc6059126b
Saša Jurić <sasa.juric@gmail.com>
2022-02-16 16:23:23 +0100

    Add boundary.visualize mix task

--- mix.exs
+++ mix.exs
@@ -63,7 +63,8 @@ defmodule Demo.MixProject do
         "credo",
         "xref graph --label compile-connected --fail-above 0",
         "dialyzer"
-      ]
+      ],
+      "boundary.visualize": ["boundary.visualize", &create_boundary_pngs/1]
     ]
   end
 
@@ -73,5 +74,25 @@ defmodule Demo.MixProject do
 
   defp dialyzer do
     [plt_add_apps: [:ex_unit, :mix]]
+  end
+
+  defp create_boundary_pngs(_args) do
+    if System.find_executable("dot") do
+      png_dir = Path.join(~w/boundary png/)
+      File.rm_rf(png_dir)
+      File.mkdir_p!(png_dir)
+
+      Enum.each(
+        Path.wildcard(Path.join("boundary", "*.dot")),
+        fn dot_file ->
+          png_file = Path.join([png_dir, "#{Path.basename(dot_file, ".dot")}.png"])
+          System.cmd("dot", ~w/-Tpng #{dot_file} -o #{png_file}/)
+        end
+      )
+
+      Mix.shell().info([:green, "Generated png files in #{png_dir}"])
+    else
+      Mix.shell().info([:yellow, "Install graphviz package to enable generation of png files."])
+    end
   end
 end
