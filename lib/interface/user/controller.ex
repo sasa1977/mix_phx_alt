ee1ad168e8a13fd0aa1693235f0029d88c965080
Saša Jurić <sasa.juric@gmail.com>
2022-02-17 12:14:20 +0100

    Improve cosmetics

--- lib/interface/user/controller.ex
+++ lib/interface/user/controller.ex
@@ -3,13 +3,13 @@
 defmodule Demo.Interface.User.Controller do
   use Demo.Interface.Controller
 
-  alias Demo.Interface.Auth
   alias Demo.Core.{Model, User}
+  alias Demo.Interface.Auth
+
+  def welcome(conn, _params), do: render(conn, :welcome, user: Auth.current_user(conn))
 
   def registration_form(conn, _params),
     do: render(conn, :registration_form, changeset: Ecto.Changeset.change(%Model.User{}))
-
-  def welcome(conn, _params), do: render(conn, :welcome, user: Auth.current_user(conn))
 
   def register(conn, %{"user" => %{"email" => email, "password" => password}}) do
     case User.register(email, password) do
