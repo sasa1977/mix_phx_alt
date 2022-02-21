8b5a9b8b2e43a2f962fbacd35ed47c83d1fd7f76
Saša Jurić <sasa.juric@gmail.com>
2022-02-21 13:16:13 +0100

    Unify token type list

--- lib/core/model/token.ex
+++ lib/core/model/token.ex
@@ -1,11 +1,18 @@
 defmodule Demo.Core.Model.Token do
   use Demo.Core.Model.Base
 
-  @type type :: :auth | :confirm_email | :password_reset
+  @all [
+    auth: 60,
+    confirm_email: 7,
+    password_reset: 1
+  ]
+
+  # generates @type :: auth | confirm_email | ... from the `@all` definition
+  @type type :: unquote(Enum.reduce(Keyword.keys(@all), &quote(do: unquote(&1) | unquote(&2))))
 
   schema "tokens" do
     field :hash, :binary
-    field :type, Ecto.Enum, values: [:auth, :confirm_email, :password_reset]
+    field :type, Ecto.Enum, values: Keyword.keys(@all)
     field :payload, :map
 
     belongs_to :user, Model.User
@@ -14,14 +21,10 @@ defmodule Demo.Core.Model.Token do
   end
 
   @spec validities :: [{type, days :: pos_integer()}]
-  def validities do
-    [
-      auth: 60,
-      confirm_email: 7,
-      password_reset: 1
-    ]
-  end
+  def validities, do: @all
 
-  @spec validity(type) :: pos_integer
-  def validity(type), do: Keyword.fetch!(validities(), type)
+  for {type, validity} <- @all do
+    @spec validity(unquote(type)) :: unquote(validity)
+    def validity(unquote(type)), do: unquote(validity)
+  end
 end
