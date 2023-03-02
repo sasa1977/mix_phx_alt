19d068f8e68f6ad8fbec86addc0ab00bae0b8150
Saša Jurić <sasa.juric@gmail.com>
2023-03-02 11:18:23 +0100

    Adapt layouts

--- lib/interface/controller.ex
+++ lib/interface/controller.ex
@@ -1,13 +1,13 @@
 defmodule Demo.Interface.Controller do
   defmacro __using__(_opts) do
     quote do
-      use Phoenix.Controller, formats: [:html]
+      use Phoenix.Controller, formats: [:html], layouts: [html: DemoWeb.Layouts]
       use Demo.Interface.Routes
 
       import Demo.Interface.User.Auth
       import Plug.Conn
 
-      plug :put_layout, {Demo.Interface.Layout.View, :app}
+      plug :put_layout, html: {Demo.Interface.Layout.HTML, :app}
 
       action_fallback Demo.Interface.Error.Controller
     end
